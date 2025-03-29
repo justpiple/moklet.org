a148adae1bc0539213ac6a6ad8fcaeeb9a50db99
Ben <me@benspace.xyz>
2025-03-29 14:24:51 +0700

    feat: improve markdown rendering performance with react-markdown

--- src/app/(main)/berita/[slug]/_components/MdViewer.tsx
+++ src/app/(main)/berita/[slug]/_components/MdViewer.tsx
@@ -1,22 +1,82 @@
-"use client";
+import Markdown, { ExtraProps } from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import { ClassAttributes, HTMLAttributes } from "react";
 
-import MDEditor from "@uiw/react-md-editor";
+import "../github-dark.min.css";
 
 interface MdViewerProps {
   markdown: string;
 }
 
-export default function MdViewer({ markdown }: Readonly<MdViewerProps>) {
+const CustomUl = ({
+  node,
+  ...props
+}: ClassAttributes<HTMLUListElement> &
+  HTMLAttributes<HTMLUListElement> &
+  ExtraProps) => (
+  <ul
+    style={{
+      display: "block",
+      listStyleType: "disc",
+      paddingInlineStart: "20px",
+    }}
+    {...props}
+  />
+);
+
+const CustomOl = ({
+  node,
+  ...props
+}: ClassAttributes<HTMLOListElement> &
+  HTMLAttributes<HTMLOListElement> &
+  ExtraProps) => (
+  <ol
+    style={{
+      display: "block",
+      paddingInlineStart: "20px",
+    }}
+    {...props}
+  />
+);
+
+const CustomPre = ({
+  node,
+  ...props
+}: ClassAttributes<HTMLPreElement> &
+  HTMLAttributes<HTMLPreElement> &
+  ExtraProps) => (
+  <pre className="not-prose rounded-xl overflow-hidden" {...props} />
+);
+
+const CustomCode = ({
+  node,
+  ...props
+}: ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps) => (
+  <code
+    className="not-prose bg-neutral-300 font-bold p-1 rounded-md"
+    {...props}
+  />
+);
+
+export function MdViewer({ markdown }: Readonly<MdViewerProps>) {
   return (
-    <MDEditor.Markdown
-      source={markdown}
-      className="prose"
-      style={{
-        backgroundColor: "#fff",
-        color: "#000",
-        fontFamily: "inherit",
-        padding: 0,
+    <Markdown
+      components={{
+        ul: CustomUl,
+        ol: CustomOl,
+        pre: CustomPre,
+        code: CustomCode,
       }}
-    />
+      className="prose"
+      rehypePlugins={[rehypeRaw, rehypeHighlight]}
+      remarkPlugins={[remarkRehype, remarkGfm]}
+    >
+      {markdown}
+    </Markdown>
   );
 }
+
+export default MdViewer;
